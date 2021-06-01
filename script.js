bed141ecdcd83a8c5050318d475b724d2b3000db
Michael Fulbright <mike.fulbright@pobox.com>
2021-05-31 20:45:14 -0400

    Change to fix final project

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+// read values from form and compute interest and report to user
+function compute() {
+    // get value of principal
+    var principal = document.getElementById("principal").value;    
+
+    // validate that the principal is greater than 0
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    // get value of interest rate
+    var rate = document.getElementById("rate").value;
+
+    // get value for # of years
+    var years = document.getElementById("years").value;
+
+    // compute the interest
+    var interest = principal * years * rate /100;
+
+    // convert # of years to the actual calendar year
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // create string to contain the HTML for the result
+    output = "If you deposit "+principal+",\<br\>";
+    output = output + "at an interest rate of "+rate+"%.\<br\>";
+    output = output + "You will receive an amount of "+interest+",\<br\>";
+    output = output + "in the year "+year+"\<br\>";
+
+    // store output HTML into the span which should contain result
+    document.getElementById("result").innerHTML = output;
 }
+
+// whenever range input changes output new value to span
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
